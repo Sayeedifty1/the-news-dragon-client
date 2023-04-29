441e9ea0a3bd336a1203cfeb8bf8499d91739025
Sayeedifty1 <md.abusayeedifty@gmail.com>
2023-04-29 22:38:16 +0600

    newscard added

--- src/pages/Home/Category/Category.jsx
+++ src/pages/Home/Category/Category.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { useLoaderData, useParams } from 'react-router-dom';
+import NewsCard from '../NewsCard/NewsCard';
+
+
 
 const Category = () => {
+    const {id} = useParams();
+    const categoryNews = useLoaderData();
     return (
         <div>
-            <h2>this is category</h2>
+            <h2>this category has :{categoryNews.length}</h2>
+            {
+                categoryNews.map(news => <NewsCard
+                key={news._id}
+                news={news}
+                ></NewsCard>)
+            }
         </div>
     );
 };
