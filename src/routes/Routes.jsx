5818870dbcd6529e49f6d89f6d49959be6b5b299
Sayeedifty1 <md.abusayeedifty@gmail.com>
2023-05-01 00:47:38 +0600

    vercel sserver added

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -46,7 +46,7 @@ const router = createBrowserRouter([
             {
                 path: ":id",
                 element:<Category></Category>,
-                loader: ({params}) => fetch(`http://localhost:5000/categories/${params.id}`)
+                loader: ({params}) => fetch(`https://the-news-dragon-server-sayeedifty1.vercel.app/categories/${params.id}`)
             },
            
 
@@ -59,7 +59,7 @@ const router = createBrowserRouter([
             {
                 path: ':id',
                 element:<PrivateRoute><News></News></PrivateRoute> ,
-                loader: ({params}) => fetch(`http://localhost:5000/news/${params.id}`)
+                loader: ({params}) => fetch(`https://the-news-dragon-server-sayeedifty1.vercel.app/news/${params.id}`)
             }
         ]
     }
